7f15034c5471c419fd3af33446f51fa185dfbc3f
Ivan Kalinin <mufnz1@gmail.com>
2026-09-25 05:04:34 +0700

    fix: keep custom frame callbacks outside radial reuse eligibility

--- src/SparkRenderer.ts
+++ src/SparkRenderer.ts
@@ -1094,6 +1094,7 @@ export class SparkRenderer<
           Object.getPrototypeOf(node) === SplatMesh.prototype &&
           node instanceof SplatMesh &&
           node.hasNativeSourceGenerator() &&
+          !node.onFrame &&
           !node.paged &&
           !node.covSplats &&
           (Object.getPrototypeOf(node.splats) === PackedSplats.prototype ||
